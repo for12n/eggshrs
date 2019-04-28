05fa5b7289e9cfcdd79473ec86a2efc1afd6931a
for12n <50074228+for12n@users.noreply.github.com>
2019-04-28 14:37:55 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif :wink:');
+
+};
